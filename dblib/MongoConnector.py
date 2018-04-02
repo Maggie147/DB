f6b97017784aab62026a17af7777f0e32ce00f4c
Xue <tianxueol@outlook.com>
2018-04-02 16:10:22 +0800

    add dblib

---
+++ dblib/MongoConnector.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+'''
+    @File        MongoConnector.py
+    @Author      tx
+    @CreatedDate 2018-04-02
+'''
+
+# from xml.etree import ElementTree
+import xml.etree.ElementTree as ET
+import pymongo, traceback
+import time
+import os
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+conffile = '../conf/SysSet.xml'
+
+class MongoConnector(object):
+    def __init__(self, conffile):
+        self.__client = None
+        self.__db = None
+        dbInfo = readConfig(conffile)
+        connectMongoDB(dbInfo)
+
+    def readConfig(self, xmlpath):
+        DB = {}
+        try:
+            tree = ET.ElementTree(file=xmlpath)
+            # tree = ET.parse(xmlpath)
+            root = tree.getroot()
+            database = root.find('database')
+            DB['db_host']  = database.find('db_address').text
+            DB['db_port'] = database.find('db_port').text
+            DB['db_name'] = database.find('db_name').text
+            DB['db_user'] = database.find('db_user').text
+            DB['db_pwd']  = database.find('db_pwd').text
+            DB['db_charset'] = 'utf8'
+        except Exception as e:
+            print("Read DataBase Config failed!!! \n error: %s \n" % e)
+            sys.exit(1)
+        return DB
+
+    def connectMongoDB(self, DB={}):
+        uri = 'mongodb://%s:%s@%s:%s/%s' % (DB['db_user'], DB['db_pwd'], DB['db_host'], DB['db_port'], DB['db_name'])
+        try:
+            self.__client = pymongo.MongoClient(host = uri, maxPoolSize = 1, socketKeepAlive = True)
+            self.__db = self.__client[DB['db_name']]
+        except Exception, e:
+            print traceback.print_exc()
+            sys.exit(1)
+
+    def __del__(self):
+        if self.__client is not None:
+            self.__client.close()
+            self.__client = None
+
+    def find(self, collection_name, query = None, field = None, sort = None, limit = 0):
+        cursor = {}
+        try:
+            collection = self.__db[collection_name]
+            cursor = collection.find(query, field, sort = sort, limit = limit)
+        except Exception, e:
+            print traceback.print_exc()
+        finally:
+            return cursor
+
+    def insert(self, collection_name, doc):
+        try:
+            collection = self.__db[collection_name]
+            collection.insert_one(doc)
+        except Exception, e:
+            print traceback.print_exc()
+
+    def update(self, collection_name, query, field, upsert = False):
+        try:
+            collection = self.__db[collection_name]
+            collection.update_one(query, field, upsert)
+        except Exception, e:
+            print traceback.print_exc()
+
+    def count(self, collection_name, query):
+        c = 0
+        try:
+            collection = self.__db[collection_name]
+            c = collection.count(query)
+        except Exception, e:
+            print traceback.print_exc()
+        finally:
+            return c
+
+    def aggregate(self, collection_name, pipeline):
+        cursor = {}
+        try:
+            collection = self.__db[collection_name]
+            cursor = collection.aggregate(pipeline)
+        except Exception, e:
+            print traceback.print_exc()
+        finally:
+            return cursor
+
+    def delete(self, collection_name, query = {}):
+        try:
+            collection = self.__db[collection_name]
+            collection.delete_many(query)
+        except Exception, e:
+            print traceback.print_exc()
+
+    def save(self, collection_name, doc):
+        try:
+            collection = self.__db[collection_name]
+            collection.save(doc)
+        except Exception, e:
+            print traceback.print_exc()
+
+    def insert_many(self, collection_name, docs):
+        results = None
+        try:
+            collection = self.__db[collection_name]
+            results = collection.insert_many(docs, False)
+        except Exception, e:
+            print traceback.print_exc()
+        finally:
+            return results
---
+++ dblib/MysqlConnector.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+'''
+    @File        MysqlConnector.py
+    @Author      tx
+    @CreatedDate 2018-04-02
+'''
+
+# from xml.etree import ElementTree
+import xml.etree.ElementTree as ET
+import MySQLdb
+import time
+import os
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+conffile = '../conf/SysSet.xml'
+
+class MysqlConnector(object):
+    def __init__(self, conffile):
+        self.conn = None
+        self.cur = None
+        dbInfo = readConfig(conffile)
+        connectMysqlDB(dbInfo)
+
+    def readConfig(self, xmlpath):
+        DB = {}
+        try:
+            tree = ET.ElementTree(file=xmlpath)
+            # tree = ET.parse(xmlpath)
+            root = tree.getroot()
+            database = root.find('database')
+            DB['db_host']  = database.find('db_address').text
+            DB['db_name'] = database.find('db_name').text
+            DB['db_user'] = database.find('db_user').text
+            DB['db_pwd']  = database.find('db_pwd').text
+            DB['db_charset'] = 'utf8'
+        except Exception as e:
+            print("Read DataBase Config failed!!! \n error: %s \n" % e)
+            sys.exit(1)
+        return DB
+
+    def connectMysqlDB(self, DB={}):
+        try:
+            self.conn = MySQLdb.connect(host    = DB['db_host'],
+                                        user    = DB['db_user'],
+                                        passwd  = DB['db_pwd'],
+                                        db      = DB['db_name'],
+                                        charset = DB['db_charset'])
+            self.cur = conn.cursor()
+            self.cursor.execute("SET NAMES utf8")
+            if not self.conn:
+                print("connect mysql failed!!!")
+                sys.exit(1)
+            print("connect mysql succeed...")
+        except MySQLdb.Error, e:
+            print("Mysql Error %d: %s" % (e.args[0], e.args[1]))
+            sys.exit(1)
+
+    def __del__(self):
+        try:
+            self.cur.close()
+            self.conn.close()
+        except Exception as e:
+            print('exit error!!!')
+
+    def create_test(self):
+        sql = "CREAT TABLE 'TUser'(\
+        'Id' int(11) unsigned NOT NULL AUTO_INCREMENT,\
+        'Name' varchar(30) NOT NULL,\
+        'Ip' char(15) NOT NULL,\
+        'Num' int(10) UNSIGNED NOT NULL,\
+        'Content' TEXT NULL,\
+        'Time' DATETIME NOT NULL,\
+        PRIMARY KEY (`Id`),\
+        INDEX `TUser_Index_IP` (`IPaddress`),\
+        )"
+        try:
+            self.cur.execute(sql)
+        except Exception as e:
+            conn.rollback()
+            print(e)
+
+
+    def insert_test(self, tablename, values):
+        sql = "insert into %s(Id, Name, Ip, Num, Content, Time) values(null, '%s', '%s', '%d', '%s', '%d')" % (\
+            tablename,\
+            MySQLdb.escape_string(str(values[0]).strip()), \
+            MySQLdb.escape_string(str(values[1]).strip()), \
+            int(str(values[2]).strip()), \
+            MySQLdb.escape_string(str(values[3]).strip()), \
+            MySQLdb.FROM_UNIXTIME(values[5].strip()))
+        try:
+            self.cur.execute(sql)
+            self.conn.commit()
+        except Exception, e:
+            conn.rollback()
+            print(traceback.print_exc())
+            print("Insert_test tablename[%s] failed!!!"%tablename)
+
+
+    def find_test(self, tablename, startid, limit=10):
+        values = None
+        # queryVar = '%'+query+'%'
+        # sql = "select * from %s where Domain like \'%s\';" % (tablename, queryVar)
+        sql = "select Id, Name, INET_NTOA(Ip), Num, (FROM_UNIXTIME)Time, from %s where Id > %d order by Id limit %d;" % (\
+            tablename, int(startid), self.limit)
+        try:
+            count = self.cursor.execute(sql)
+            values = self.cursor.fetchall()
+            if not values:
+                print("not more data!!!")
+        except Exception, e:
+            conn.rollback()
+            print(traceback.print_exc())
+            print("find failed!!!")
+        # begin_id += count
+        return values
+
+
+    def find_one_test(self, tablename, id):
+        sql = "select * from %s where Id = %s" % (tablename, id)
+        try:
+            self.cursor.execute(sql)
+            values = self.cursor.fetchone()
+        except Exception as e:
+            conn.rollback()
+            print(e)
+        return values
+
+    def get_last_test(self, tablename):
+        num = 0
+        sql = "select Num from %s order by MailId desc limit 1"%tablename
+        try:
+            self.cursor.execute(sql)
+            result = self.cursor.fetchall()
+            if result:
+                num = result[0][0]
+        except Exception as e:
+            conn.rollback()
+            print(e)
+        return num
